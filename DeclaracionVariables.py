64ab9f9a06755488634a56803ddd2717e9bdf948
gaalcemo <gacelis89@gmail.com>
2024-04-30 20:14:55 +0200

    V1.3

---
+++ DeclaracionVariables.py
@@ -0,0 +1,33 @@
+# Declaracion de Variables numericas entera
+n_edad = 47
+numero = n_edad + 1
+
+#Declaracion de variables numericas de coma flotante
+n_numero = 23.5245
+
+#Declaracion de variables de tipo String
+nombre = "Gabriel"
+
+#Declaracion de variables de tipo string varias lineas
+s_textoLargo = "Esto es una mensaje\n con tres saltos\n de linea"
+
+#Declaracion de constantes, las constantes se declaran en MAYUSCULAS
+NUMEROPI = 3.14159
+
+#Declaracion de BOOLEANS, debe llevar is_ antes de la variable, no es obligatorio
+is_verdadero = True
+is_casado = False
+
+#True - 1 False - 0
+True ==1
+False ==0
+
+#Imprimir variables en pantalla
+print (n_edad)
+print (numero)
+print (nombre)
+print (n_numero)
+print (s_textoLargo)
+print (NUMEROPI)
+print (True+5)
+print (is_verdadero)
